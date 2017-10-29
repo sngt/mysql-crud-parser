0e8c8e0efe1ec99ebe5c6ab5883e05b1914542bf
toyoshima_shingo <goradd@gmail.com>
2017-10-29 15:39:52 +0900

    implemented update.

--- test/test.sql
+++ test/test.sql
@@ -4,7 +4,7 @@ Block comments start with '/*' are also supported.
 */
 SELECT id, `name` FROM `user` WHERE id IN (1, 2)\G -- ';' and '\G' are both recognized as statement delimiters.
 Delete From `user` Where id=1; #Reserved words are NOT case sensitive.
-update `user` set name = 'scipio' where id = 2 and name = 'paullus';
+update `user` set name = 'scipio' where id = 2 and name in ('paullus', 'varro');
 
 INSERT INTO `user` VALUES (3, 'hamilcar', 'a722c63db8ec8625af6cf71cb8c2d939');
 INSERT INTO `user` (id, `name`, `password`) VALUE (4, 'hasdrubal', 'c1572d05424d0ecb2a65ec6a82aeacbf');
